5079507d18d7555fd0bc68ab7e82ca855f199968
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2023-09-16 03:14:14 +0530

    commited

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# thread-clone
+# Threads Clone Tutorial
+
+### Built With React, MongoDB, ExpressJS, NodeJS, JWT, Cloudinary
+
+# [Tutorial]
+
+![Screenshot of App](https://i.ibb.co/Gs9fWwh/Group-8.png)
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,51 @@
+import { Flex, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import Post from "../components/Post";
+import { useRecoilState } from "recoil";
+import postsAtom from "../atoms/postsAtom";
+
+const HomePage = () => {
+	const [posts, setPosts] = useRecoilState(postsAtom);
+	const [loading, setLoading] = useState(true);
+	const showToast = useShowToast();
+	useEffect(() => {
+		const getFeedPosts = async () => {
+			setLoading(true);
+			setPosts([]);
+			try {
+				const res = await fetch("/api/posts/feed");
+				const data = await res.json();
+				if (data.error) {
+					showToast("Error", data.error, "error");
+					return;
+				}
+				console.log(data);
+				setPosts(data);
+			} catch (error) {
+				showToast("Error", error.message, "error");
+			} finally {
+				setLoading(false);
+			}
+		};
+		getFeedPosts();
+	}, [showToast, setPosts]);
+
+	return (
+		<>
+			{!loading && posts.length === 0 && <h1>Follow some users to see the feed</h1>}
+
+			{loading && (
+				<Flex justify='center'>
+					<Spinner size='xl' />
+				</Flex>
+			)}
+
+			{posts.map((post) => (
+				<Post key={post._id} post={post} postedBy={post.postedBy} />
+			))}
+		</>
+	);
+};
+
+export default HomePage;
